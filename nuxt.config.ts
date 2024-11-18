a0bb09f1edbf2a252279b3283adb278e21a9bba9
MichaelHo <michelo851a1203@gmail.com>
2024-11-18 11:24:25 +0800

    fix(): baseurl # 2024-11-18-001
    	修改：     nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/chris-test/'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@unocss/nuxt'],
