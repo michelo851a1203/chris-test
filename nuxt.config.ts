47e8ec694a906035eac5eb5345238f19aac4d7ed
MichaelHo <michelo851a1203@gmail.com>
2024-11-18 11:36:37 +0800

    fix(): issue # 2024-11-18-002
    	修改：     bun.lockb
    	修改：     nuxt.config.ts
    	修改：     package.json
    	新檔案：   public/.nojekyll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/chris-test/'
+    baseURL: process.env.NODE_ENV === 'production' ? '/chris-test/' : '/',
+    buildAssetsDir: '/static/'
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
